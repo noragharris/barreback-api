219fb6aad308bcf51ce6393fafb86b08cddb0248
Nora Harris <noragharris@gmail.com>
2021-07-07 23:01:16 -0500

    work to test out separate model for equipment

---
+++ app/controllers/equipment_items_controller.rb
@@ -0,0 +1,43 @@
+class EquipmentItemsController < ApplicationController
+  before_action :set_equipment_item, only: %i[show update destroy]
+
+  # GET /equipment_items
+  def index
+    @equipment_items = EquipmentItem.all
+    json_response(@equipment_items)
+  end
+
+  # GET /equipment_items/:id
+  def show
+    json_response(@equipment_item)
+  end
+
+  # POST /equipment_items
+  def create
+    @equipment_item = EquipmentItem.create!(equipment_item_params)
+    json_response(@equipment_item, :created)
+  end
+
+  # PUT /equipment_items/:id
+  def update
+    @equipment_item.update(equipment_item_params)
+    json_response(@equipment_item)
+  end
+
+  # DELETE /equipment_items/:id
+  def destroy
+    @equipment_item.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_equipment_item
+    @equipment_item = EquipmentItem.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def equipment_item_params
+    params.permit(:name)
+  end
+end
---
+++ db/migrate/20210708024753_create_equipment_items.rb
@@ -0,0 +1,9 @@
+class CreateEquipmentItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :equipment_items do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
